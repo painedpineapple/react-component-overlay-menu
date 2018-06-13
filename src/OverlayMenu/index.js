486503fa824ff74740b71e8e1caac3f9262f111c
Logan Call <this@logancall.com>
2018-06-13 14:50:59 -0600

    Component now accepts a isActive prop and tracks scrollTop

--- src/OverlayMenu/index.js
+++ src/OverlayMenu/index.js
@@ -9,6 +9,7 @@ const AnimatedContainer = animated(Container)
 
 type tProps = {
   options: {
+    isActive: boolean,
     items: Array<any>,
     rootId?: string,
     styles?: {},
@@ -18,10 +19,17 @@ type tProps = {
   itemRender: (item: any, styles: any) => any,
 }
 
-export default class OverlayMenu extends React.Component<tProps> {
+type tState = {
+  scrollTop: number,
+}
+
+export default class OverlayMenu extends React.Component<tProps, tState> {
   root: any
   mount: any
   rootId: string
+  state = {
+    scrollTop: 0,
+  }
   constructor(props: tProps) {
     super(props)
 
@@ -34,19 +42,42 @@ export default class OverlayMenu extends React.Component<tProps> {
   componentDidMount() {
     if (typeof document !== 'undefined') {
       this.root.appendChild(this.mount)
-      document
-        .getElementsByTagName('body')[0]
-        .classList.add('component-overlay-menu-active')
     }
   }
   componentWillUnmount() {
     if (typeof document !== 'undefined') {
       this.root.removeChild(this.mount)
-      document
-        .getElementsByTagName('body')[0]
-        .classList.remove('component-overlay-menu-active')
     }
   }
+
+  getSnapshotBeforeUpdate(prevProps) {
+    if (typeof document !== 'undefined') {
+      // menu was active, but is about to not be
+      if (prevProps.options.isActive && !this.props.options.isActive) {
+        document
+          .getElementsByTagName('body')[0]
+          .classList.remove('component-overlay-menu-active')
+        // menu wasn't active, but is about to be
+      } else if (!prevProps.options.isActive && this.props.options.isActive) {
+        document
+          .getElementsByTagName('body')[0]
+          .classList.add('component-overlay-menu-active')
+      }
+    }
+  }
+
+  handleScroll = () => {
+    if (!this.props.options.isActive) {
+      const scrollTop =
+        window.pageYOffset ||
+        (document.documentElement ? document.documentElement.scrollTop : 0)
+
+      this.setState({
+        scrollTop,
+      })
+    }
+  }
+
   render() {
     const {
       options,
@@ -55,34 +86,38 @@ export default class OverlayMenu extends React.Component<tProps> {
       belowMenuRender,
       ...props
     } = this.props
-    const { items, rootId } = options
-    return ReactDOM.createPortal(
-      <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} native>
-        {styles => (
-          <AnimatedContainer
-            style={styles}
-            options={{
-              ...options,
-              styles: options.styles || {},
-            }}
-            {...props}
-          >
-            {aboveMenuRender && aboveMenuRender()}
-            <nav>
-              <Trail
-                from={{ opacity: 0 }}
-                to={{ opacity: 1 }}
-                keys={items.map(item => item.id)}
-              >
-                {items.map(item => styles => itemRender({ item, styles }))}
-              </Trail>
-            </nav>
-            {belowMenuRender && belowMenuRender()}
-          </AnimatedContainer>
-        )}
-      </Spring>,
-      // $FlowFixMe
-      this.mount,
+    const { items, rootId, isActive } = options
+    return isActive ? (
+      ReactDOM.createPortal(
+        <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} native>
+          {styles => (
+            <AnimatedContainer
+              style={styles}
+              options={{
+                ...options,
+                styles: options.styles || {},
+              }}
+              {...props}
+            >
+              {aboveMenuRender && aboveMenuRender()}
+              <nav>
+                <Trail
+                  from={{ opacity: 0 }}
+                  to={{ opacity: 1 }}
+                  keys={items.map(item => item.id)}
+                >
+                  {items.map(item => styles => itemRender({ item, styles }))}
+                </Trail>
+              </nav>
+              {belowMenuRender && belowMenuRender()}
+            </AnimatedContainer>
+          )}
+        </Spring>,
+        // $FlowFixMe
+        this.mount,
+      )
+    ) : (
+      <span />
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ class App extends React.Component<{}, { isActive: boolean }> {
         <button
           onClick={this.handleClick}
           style={{
-            position: 'absolute',
+            position: 'sticky',
             right: 30,
             top: 30,
             zIndex: '9999999999999',
@@ -51,27 +51,26 @@ class App extends React.Component<{}, { isActive: boolean }> {
             </p>
           ))}
         </div>
-        {this.state.isActive && (
-          <OverlayMenu
-            options={{
-              items,
-              styles: overlayStyles,
-            }}
-            itemRender={({ item, styles }) => (
-              <a style={styles} href={item.url}>
-                {item.title}
-              </a>
-            )}
-            aboveMenuRender={() => (
-              <a href="/" className="logo-wrapper">
-                <Logo />
-              </a>
-            )}
-            belowMenuRender={() => (
-              <div style={{ margin: 60 }}>I will appear below the menu</div>
-            )}
-          />
-        )}
+        <OverlayMenu
+          options={{
+            isActive: this.state.isActive,
+            items,
+            styles: overlayStyles,
+          }}
+          itemRender={({ item, styles }) => (
+            <a style={styles} href={item.url}>
+              {item.title}
+            </a>
+          )}
+          aboveMenuRender={() => (
+            <a href="/" className="logo-wrapper">
+              <Logo />
+            </a>
+          )}
+          belowMenuRender={() => (
+            <div style={{ margin: 60 }}>I will appear below the menu</div>
+          )}
+        />
       </div>
     )
   }
